39cfd92e4c2b0910f5bfa77974a22a9ccc2ecaf9
caracena <claudio.aracena2@gmail.com>
2016-04-20 18:55:53 +1000

    get training data

--- classifier/assignment.py
+++ classifier/assignment.py
@@ -1,7 +1,24 @@
-
+import csv
 
 def main():
-    pass
+    content = []
+    with open('assignment1_2016S1/training_data.csv') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            content.append(row)
+
+    X_train = [x[1:] for x in content]
+
+    content = []
+    with open('assignment1_2016S1/training_labels.csv') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            content.append(row)
+
+    y_train = [y[1] for y in content]
+
+    
+
 
 
 
