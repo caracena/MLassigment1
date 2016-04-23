1bdb1cd883c996879eab147eddc0feb0bd070806
caracena <claudio.aracena2@gmail.com>
2016-04-23 18:12:07 +1000

    add load data

--- classifier/assignment.py
+++ classifier/assignment.py
@@ -1,25 +1,43 @@
-import csv
+import csv, random
+import numpy as np
 
 def main():
-    content = []
-    with open('assignment1_2016S1/training_data.csv') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            content.append(row)
+    X_train, y_train, X_test = load_data()
 
+def load_data():
+
+    content = extract_data('../assignment1_2016S1/training_data.csv')
     X_train = [x[1:] for x in content]
+    X_train = np.asarray(X_train, dtype='f')
 
-    content = []
-    with open('assignment1_2016S1/training_labels.csv') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            content.append(row)
-
+    content = extract_data('../assignment1_2016S1/training_labels.csv')
     y_train = [y[1] for y in content]
 
-    
+    content = extract_data('../assignment1_2016S1/test_data.csv')
+    X_test = [x[1:] for x in content]
+    X_test = np.asarray(X_test, dtype='f')
 
+    return X_train, y_train, X_test
 
+def extract_data(filename):
+
+    content = []
+    with open(filename) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            content.append(row)
+    return content
+
+def cross_validation(k, m):
+    items = list(range(m))
+    random.shuffle(items)
+    slices = [items[i::k] for i in range(k)]
+    for i in range(k):
+        validation = slices[i]
+        training = [item
+                    for s in slices if s is not validation
+                    for item in s]
+        yield training, validation
 
 
 if __name__ == "__main__":
